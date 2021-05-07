08d81d9d63f01a2d106c1305e8c16cc7c5bb3d7c
Praveen Bandarage <bandarprav@ecs.vuw.ac.nz>
2021-05-07 13:53:36 +1200

    Initial commit with base layout using tailwind

---
+++ public/scripts.js
@@ -0,0 +1,9 @@
+//add event listeners
+
+const btn = document.querySelector('button.mobile-menu-button');
+const menu = document.querySelector('.mobile-menu');
+
+btn.addEventListener("click", () => {
+menu.classList.toggle("hidden");
+
+});
